2837266f80dafcac81b1b2eaba0e65054f4b7537
PolarDefend <patch.demon115@gmail.com>
2023-12-13 22:59:49 -0800

    add components and routes

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import More from './components/More';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/more" element={<More />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
