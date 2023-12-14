13dfe801db423b831bcb8d75eb4294b283f14a02
PolarDefend <patch.demon115@gmail.com>
2023-12-14 07:02:25 -0800

    add header and styling to Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Contact() {
     return (
-        <p></p>
+        <div className="contact">
+            <h1>Let's start a project together!</h1>
+            <p>For all inquiries, or to schedule an interview, please fill out the form below:</p>
+        </div>
     )
 }
 
