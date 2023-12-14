f54cc1ff8643b8bb826a2374f6cf44e5e08eac52
PolarDefend <patch.demon115@gmail.com>
2023-12-14 08:00:38 -0800

    set up form shell

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,61 @@ function Contact() {
         <div className="contact">
             <h1>Let's start a project together!</h1>
             <p>For all inquiries, or to schedule an interview, please fill out the form below:</p>
+            <br/>
+            <form>
+                <label>Full Name:</label>
+                <input 
+                    required 
+                    name="name" 
+                    type="text" 
+                    placeholder="Name"
+                >
+                </input>
+                <br/>
+                <label>Company (Optional):</label>
+                <input
+                    name="company"
+                    type="text"
+                    placeholder="Company"
+                >                
+                </input>
+                <br/>
+                <label>Email:</label>
+                <input
+                    required
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                >
+                </input>
+                <br/>
+                <label>Phone:</label>
+                <input 
+                    required
+                    name="phone"
+                    type="text"
+                    placeholder="Phone"
+                >
+                </input>
+                <br/>
+                <label>Subject:</label>
+                <input
+                    required
+                    name="subject"
+                    type="text"
+                    placeholder="Subject"
+                >
+                </input>
+                <br/>
+                <label>Message:</label>
+                <textarea
+                    required
+                    name="message"
+                    type="text"
+                    placeholder="Message..."
+                >
+                </textarea>
+            </form>
         </div>
     )
 }
