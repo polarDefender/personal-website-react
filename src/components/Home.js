676cc5f1e84a5ff8afd80c9d967bebfa90a2f871
PolarDefend <patch.demon115@gmail.com>
2023-12-18 05:20:11 -0800

    add margins to left and right of divs; add exclamation point to Home paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div className="about">
             <h1>Hi! My name is Charlie Chase Gray!</h1>
-            <p>As a skilled Front-End React Developer, I excel in creating dynamic, responsive web applications with seamless user experiences. Utilizing React's robust capabilities, I develop intuitive interfaces, enhance performance, and apply cutting-edge design patterns. My expertise lies in transforming complex challenges into user-friendly, visually appealing solutions that perform flawlessly across diverse platforms and devices. Join me in blending creativity with technology to innovate and inspire.</p>
+            <p>As a skilled Front-End React Developer, I excel in creating dynamic, responsive web applications with seamless user experiences. Utilizing React's robust capabilities, I develop intuitive interfaces, enhance performance, and apply cutting-edge design patterns. My expertise lies in transforming complex challenges into user-friendly, visually appealing solutions that perform flawlessly across diverse platforms and devices. Join me in blending creativity with technology to innovate and inspire!</p>
         </div>
     )
 }
