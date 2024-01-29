280827bff57460124da67ab22f931f01db1a1584
PolarDefend <patch.demon115@gmail.com>
2024-01-29 11:18:59 -0700

    fix paragraph width to 90% globally; left/right margins 5%; centered additional services list

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -51,7 +51,7 @@ function Pricing() {
             </div>
             <div className="additional-services">
                 <h1>Additional Services</h1>
-                <ul className="details">
+                <ul className="outer-list">
                     <li className="header-li">Project Maintenance</li>
                         <ul>
                             <li>$25 per month</li>
