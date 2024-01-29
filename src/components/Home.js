9365962a26be902ee0015bae88e9a84f7b750bdb
PolarDefend <patch.demon115@gmail.com>
2024-01-29 00:11:02 -0700

    remove contraction and repalce with 'does not' in about paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ function Home() {
                 <br/>
                 <p>Hello, and welcome to Charlie Chase Productions, the center for all of my software development endeavors! I'm Charlie, a passionate full stack developer with a flair for creating dynamic and user-friendly web applications. My journey in software development is marked by a deep expertise in JavaScript, with an exceptional skill set in React, making me adept at crafting responsive and interactive user interfaces. React isn't just a tool for me; it's a canvas where I blend functionality with creativity.</p>
 
-                <p>My proficiency doesn't stop at front-end technologies. I'm equally skilled in Python, utilizing its powerful and efficient nature to develop robust back-end solutions. Combined with Flask, I create seamless and scalable server-side applications. Moreover, my knowledge of SQL ensures that data management and manipulation in my applications are both efficient and secure.</p>
+                <p>My proficiency does not stop at front-end technologies. I'm equally skilled in Python, utilizing its powerful and efficient nature to develop robust back-end solutions. Combined with Flask, I create seamless and scalable server-side applications. Moreover, my knowledge of SQL ensures that data management and manipulation in my applications are both efficient and secure.</p>
 
                 <p>Dive into my portfolio to see how I bring together JavaScript, React, Python, Flask, and SQL to build seamless, full-stack solutions that not only meet but exceed user expectations.</p>
                 <br/>
